914ec9a9b1535ff6fdae7b391f2b66fb261782a7
@tjbade <tjbade@gmail.com>
2019-01-09 13:21:49 -0500

    added beans

---
+++ src/main/java/com/revature/beans/File.java
@@ -0,0 +1,117 @@
+package com.revature.beans;
+
+import java.sql.Date;
+
+public class File {
+	private int id;
+	private String filename;
+	private String filetype;
+	private Date added;
+	private double filesize;
+	private String sharestr;
+	private Folder parent;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getFiletype() {
+		return filetype;
+	}
+	public void setFiletype(String filetype) {
+		this.filetype = filetype;
+	}
+	public Date getAdded() {
+		return added;
+	}
+	public void setAdded(Date added) {
+		this.added = added;
+	}
+	public double getFilesize() {
+		return filesize;
+	}
+	public void setFilesize(double filesize) {
+		this.filesize = filesize;
+	}
+	public String getSharestr() {
+		return sharestr;
+	}
+	public void setSharestr(String sharestr) {
+		this.sharestr = sharestr;
+	}
+	public Folder getParent() {
+		return parent;
+	}
+	public void setParent(Folder parent) {
+		this.parent = parent;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((added == null) ? 0 : added.hashCode());
+		result = prime * result + ((filename == null) ? 0 : filename.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(filesize);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((filetype == null) ? 0 : filetype.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		result = prime * result + ((sharestr == null) ? 0 : sharestr.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		File other = (File) obj;
+		if (added == null) {
+			if (other.added != null)
+				return false;
+		} else if (!added.equals(other.added))
+			return false;
+		if (filename == null) {
+			if (other.filename != null)
+				return false;
+		} else if (!filename.equals(other.filename))
+			return false;
+		if (Double.doubleToLongBits(filesize) != Double.doubleToLongBits(other.filesize))
+			return false;
+		if (filetype == null) {
+			if (other.filetype != null)
+				return false;
+		} else if (!filetype.equals(other.filetype))
+			return false;
+		if (id != other.id)
+			return false;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		if (sharestr == null) {
+			if (other.sharestr != null)
+				return false;
+		} else if (!sharestr.equals(other.sharestr))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "File [id=" + id + ", filename=" + filename + ", filetype=" + filetype + ", added=" + added
+				+ ", filesize=" + filesize + ", sharestr=" + sharestr + ", parent=" + parent + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/revature/beans/Folder.java
@@ -0,0 +1,76 @@
+package com.revature.beans;
+
+public class Folder {
+	private int id;
+	private User owner;
+	private String name;
+	private Folder parent;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public User getOwner() {
+		return owner;
+	}
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Folder getParent() {
+		return parent;
+	}
+	public void setParent(Folder parent) {
+		this.parent = parent;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Folder other = (Folder) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Folder [id=" + id + ", owner=" + owner + ", name=" + name + ", parent=" + parent + "]";
+	}
+	
+	
+}
