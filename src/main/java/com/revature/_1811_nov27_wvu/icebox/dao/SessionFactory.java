904061cef3e631fe00ba894bf866b727bf0ef4cc
Mateusz Wiater <mateuszwiater@gmail.com>
2019-01-09 02:38:09 -0500

    Configured Hibernate entirely in Java.

---
+++ src/main/java/com/revature/_1811_nov27_wvu/icebox/dao/SessionFactory.java
@@ -0,0 +1,41 @@
+package com.revature._1811_nov27_wvu.icebox.dao;
+
+import java.util.Properties;
+
+import org.hibernate.Session;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.springframework.stereotype.Component;
+
+import com.revature._1811_nov27_wvu.icebox.entity.Address;
+
+@Component
+public class SessionFactory {
+	private org.hibernate.SessionFactory sf;
+
+	public SessionFactory() {
+		// Set the Hibernate configuration
+		Properties settings = new Properties();
+		settings.put(Environment.DRIVER, "oracle.jdbc.OracleDriver");
+		settings.put(Environment.DIALECT, "org.hibernate.dialect.Oracle10gDialect");
+		settings.put(Environment.URL, "jdbc:oracle:thin:@localhost:1521:xe");
+		settings.put(Environment.USER, "icebox");
+		settings.put(Environment.PASS, "p4ssw0rd");
+		settings.put(Environment.POOL_SIZE, 35);
+		settings.put(Environment.SHOW_SQL, true);
+		settings.put(Environment.FORMAT_SQL, true);
+		settings.put(Environment.IMPLICIT_NAMING_STRATEGY, ImplicitNamingStrategyJpaCompliantImpl.INSTANCE);
+		
+		// Create the SessionFactory with the configuration
+		sf = new Configuration()
+				.setProperties(settings)
+				.addAnnotatedClass(Address.class)
+				.addPackage("com.revature._1811_nov27_wvu.icebox")
+				.buildSessionFactory();
+	}
+	
+	public Session getSession() {
+		return sf.openSession();
+	}	
+}
