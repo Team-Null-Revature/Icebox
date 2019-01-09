d9495a29d413338684eb5ec0cd8247f3ab0bfdcf
Mateusz Wiater <mateuszwiater@gmail.com>
2019-01-09 02:37:52 -0500

    Configured Spring entirely in Java.

---
+++ src/main/java/com/revature/_1811_nov27_wvu/icebox/Icebox.java
@@ -0,0 +1,45 @@
+package com.revature._1811_nov27_wvu.icebox;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Icebox implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// Create the annotated spring context
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(SpringConfig.class);
+
+		// Add the annotated spring context to the current servlet context
+		servletContext.addListener(new ContextLoaderListener(ctx));
+		
+		// Map the dispatcher servlet to the root path
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
+	
+	@EnableWebMvc
+	@Configuration
+	@ComponentScan(basePackages = "com.revature._1811_nov27_wvu.icebox")
+	public static class SpringConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			// Add the static files location
+			registry.addResourceHandler("/static/**").addResourceLocations("/static/");	
+		}
+	}
+}
