a6479c556f9e84546a0f16016902a27e74f8c6f5
Chris Pham <cpham1994@gmail.com>
2019-01-10 13:55:47 -0500

    login updated - chris pham

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent,
+    AppComponent, 
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/main/java/com/revature/_1811_nov27_wvu/icebox/services/UserService.java
@@ -0,0 +1,7 @@
+package com.revature._1811_nov27_wvu.icebox.services;
+
+import com.revature._1811_nov27_wvu.icebox.entity.User;
+
+public interface UserService {
+	public User login(String user, String pass);
+}
