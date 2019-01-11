0dd4dde1dc0ee8de064f266c2c4c42d6899ab670
David Donnelly <davidonnelly25@gmail.com>
2019-01-10 21:35:04 -0500

    Removed @Service from Interface

--- src/main/java/com/revature/_1811_nov27_wvu/icebox/services/UserService.java
+++ src/main/java/com/revature/_1811_nov27_wvu/icebox/services/UserService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import com.revature._1811_nov27_wvu.icebox.entity.User;
 
-@Service
 public interface UserService {
 	User addUser(User u); 
 	User getUserById(int i);
