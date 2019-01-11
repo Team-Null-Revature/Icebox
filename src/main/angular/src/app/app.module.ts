4123b6d76fec60778b0607273f7e0da6e4f1caaa
dehkrueger <drrueger@wpi.edu>
2019-01-11 15:11:23 -0500

    beginning addFolder functionality in folder component

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FolderComponent } from './folder/folder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FolderComponent
   ],
   imports: [
     BrowserModule,
