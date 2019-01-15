f7a13950303237741cfd5aec0a37c2fc1b3f2409
Mateusz Wiater <mateuszwiater@gmail.com>
2019-01-15 09:01:59 -0500

    Updated DB.

--- src/main/resources/Bootstrap.sql
+++ src/main/resources/Bootstrap.sql
@@ -48,52 +48,52 @@ DROP SEQUENCE Folder_Seq;
    Create Tables
 ********************************************************************************/
 CREATE TABLE Ib_User (
-    User_Id number(10) PRIMARY KEY,
-    Username varchar2(30) NOT NULL,
-    Pass varchar2(30) NOT NULL,
-    FName varchar2(30) NOT NULL,
-    LName varchar2(30) NOT NULL,
-    Email varchar2(50) NOT NULL
+    User_Id NUMBER(10) PRIMARY KEY,
+    Username VARCHAR2(30) NOT NULL,
+    Pass VARCHAR2(30) NOT NULL,
+    FName VARCHAR2(30) NOT NULL,
+    LName VARCHAR2(30) NOT NULL,
+    Email VARCHAR2(50) NOT NULL
 );
 
 CREATE TABLE Ib_Folder (
-    Folder_Id number(10) PRIMARY KEY,
-    Owner number(10), --fk
-    Name varchar2(100) NOT NULL,
-    P_Folder number(10), --fk
+    Folder_Id NUMBER(10) PRIMARY KEY,
+    Owner NUMBER(10), --fk
+    Name VARCHAR2(100) NOT NULL,
+    P_Folder NUMBER(10), --fk
     CONSTRAINT FK_Fo_Owner FOREIGN KEY (Owner) REFERENCES Ib_User(User_Id),
     CONSTRAINT FK_Fo_Parent FOREIGN KEY (P_Folder) REFERENCES Ib_Folder(Folder_Id)
 );
 
 CREATE TABLE Ib_File (
-    File_Id number(10) PRIMARY KEY,
-    FileName varchar(100) NOT NULL,
-    FileType varchar(20) NOT NULL,
-    Added date NOT NULL,
-    FileSize number(20,10) NOT NULL, --this should be the size in bytes, we can calculate later
-    ShareStr varchar2(20),
-    P_Folder number(10), --fk
+    File_Id NUMBER(10) PRIMARY KEY,
+    FileName VARCHAR2(100) NOT NULL,
+    FileType VARCHAR2(20) NOT NULL,
+    Added DATE NOT NULL,
+    FileSize NUMBER(20,10) NOT NULL, --this should be the size in bytes, we can calculate later
+    ShareStr VARCHAR2(36),
+    P_Folder NUMBER(10), --fk
     CONSTRAINT FK_FI_folder FOREIGN KEY (P_Folder) REFERENCES Ib_Folder(folder_id)
 );
 
 CREATE TABLE Ib_Comment (
-    Comment_Id number(10) PRIMARY KEY,
-    User_Id number(10) NOT NULL,--fk
-    File_Id number(10) NOT NULL,--fk
-    CommentStr varchar2(1000) NOT NULL,
+    Comment_Id NUMBER(10) PRIMARY KEY,
+    User_Id NUMBER(10) NOT NULL,--fk
+    File_Id NUMBER(10) NOT NULL,--fk
+    CommentStr VARCHAR2(1000) NOT NULL,
     CONSTRAINT FK_C_User FOREIGN KEY (User_Id) REFERENCES Ib_User(user_id),
     CONSTRAINT FK_C_File FOREIGN KEY (File_Id) REFERENCES Ib_File(file_id)
 );
 
 CREATE TABLE Ib_Tag (
-    Tag_Id number(10) PRIMARY KEY,
-    Name varchar2(20) NOT NULL
+    Tag_Id NUMBER(10) PRIMARY KEY,
+    Name VARCHAR2(20) NOT NULL
 );
 
 CREATE TABLE Ib_File_tag (
-    File_Tag_Id number(10) PRIMARY KEY,
-    File_Id number(10) NOT NULL, --fk
-    Tag_Id number(10) NOT NULL, --fk
+    File_Tag_Id NUMBER(10) PRIMARY KEY,
+    File_Id NUMBER(10) NOT NULL, --fk
+    Tag_Id NUMBER(10) NOT NULL, --fk
     CONSTRAINT FK_FT_Tag FOREIGN KEY (Tag_Id) REFERENCES Ib_Tag(tag_id),
     CONSTRAINT FK_FT_Tile FOREIGN KEY (File_Id) REFERENCES Ib_File(file_id)
 );
